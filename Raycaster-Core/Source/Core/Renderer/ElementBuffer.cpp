ee0ffc2b4101750e005b09f13edb61ae42d86ca2
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2024-11-16 19:02:15 +0200

    Implemented dynamic ElementBuffer creation and data setting

--- Raycaster-Core/Source/Core/Renderer/ElementBuffer.cpp
+++ Raycaster-Core/Source/Core/Renderer/ElementBuffer.cpp
@@ -11,6 +11,14 @@ namespace Core {
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), data, GL_STATIC_DRAW);
 	}
 
+	ElementBuffer::ElementBuffer(uint32_t count)
+		: m_RendererID(0), m_Count(0)
+	{
+		glGenBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), nullptr, GL_DYNAMIC_DRAW);
+	}
+
 	ElementBuffer::~ElementBuffer() {
 		glDeleteBuffers(1, &m_RendererID);
 	}
@@ -21,5 +29,12 @@ namespace Core {
 
 	void ElementBuffer::Unbind() const {
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+	void ElementBuffer::SetData(const uint32_t* data, uint32_t count) {
+		m_Count = count;
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+		glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, count * sizeof(uint32_t), data);
 	}
 }
--- Raycaster-Core/Source/Core/Renderer/ElementBuffer.h
+++ Raycaster-Core/Source/Core/Renderer/ElementBuffer.h
@@ -9,11 +9,14 @@ namespace Core {
 		uint32_t m_RendererID;
 		uint32_t m_Count;
 	public:
-		ElementBuffer(const uint32_t* data, uint32_t count);
+		ElementBuffer(const uint32_t* data, uint32_t count); 
+		ElementBuffer(uint32_t size);
 		~ElementBuffer();
 
 		void Bind() const;
 		void Unbind() const;
+
+		void SetData(const uint32_t* data, uint32_t count);
 
 		inline uint32_t GetCount() { return m_Count; }
 	};
