5d5ae64263f106738d77d030beb87172ebe51675
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-07-20 23:09:32 +0300

    Implemented Interactables using a variation of the flyweight pattern

---
+++ Raycaster-App/Source/Interactables.cpp
@@ -0,0 +1,166 @@
+#include "Interactables.h"
+
+#include <array>
+
+static constexpr InteractionResult DebugInteraction() {
+    return { InteractionResult::Type::Debug, "You interacted with an interactable" };
+}
+
+using InteractionPtr = InteractionResult(*)();
+
+enum class PlacementType {
+    Centre = 0,
+    Floor,
+    Ceiling
+};
+
+struct InteractableParameters {
+    InteractionPtr Interaction = nullptr;
+    glm::vec3 Scale{};
+    uint32_t AtlasIndex = 0;
+    PlacementType Placement = PlacementType::Floor;
+};
+
+constinit std::array<InteractableParameters, InteractableType::ENUMERATION_MAX + 1> s_InteractableParameters = []{
+    std::array<InteractableParameters, InteractableType::ENUMERATION_MAX + 1> parameters;
+    parameters[InteractableType::Light] = InteractableParameters{
+        .Scale = {0.5f, 0.5f, 0.5f},
+        .AtlasIndex = 10,
+        .Placement = PlacementType::Ceiling,
+    };
+    parameters[InteractableType::Barrel] = InteractableParameters{
+        .Interaction = DebugInteraction,
+        .Scale = {0.5f, 0.5f, 0.5f},
+        .AtlasIndex = 8,
+        .Placement = PlacementType::Floor
+    };
+    return parameters;
+    }();
+
+static constexpr uint32_t GetAtlasIndex(InteractableType::Enumeration type) {
+    if (type > InteractableType::ENUMERATION_MAX) {
+        return 0;
+    }
+
+    return s_InteractableParameters[type].AtlasIndex;
+}
+
+static constexpr InteractionPtr GetInteraction(InteractableType::Enumeration type) {
+    if (type > InteractableType::ENUMERATION_MAX) {
+        return nullptr;
+    }
+
+    return s_InteractableParameters[type].Interaction;
+}
+
+static constexpr glm::vec3 GetScale(InteractableType::Enumeration type) {
+    if (type > InteractableType::ENUMERATION_MAX) {
+        return glm::vec3(1.0f);
+    }
+
+    return s_InteractableParameters[type].Scale;
+}
+
+static constexpr float CalculatePositionZ(InteractableType::Enumeration type, float height) {
+    if (type > InteractableType::ENUMERATION_MAX) {
+        return 0.5f;
+    }
+
+    switch (s_InteractableParameters[type].Placement) {
+    case PlacementType::Centre:
+        return 0.5f;
+    case PlacementType::Floor:
+        return height * 0.5f;
+    case PlacementType::Ceiling:
+        return 1.0f - height * 0.5f;
+    }
+}
+
+void Interactables::Init() {
+    m_CachedPosition = { -1.0f, -1.0f };
+    m_CachedIndex = -1;
+}
+
+void Interactables::Shutdown() {
+    m_Interactables = std::vector<Interactable>();
+}
+
+void Interactables::Add(InteractableType::Enumeration type, glm::vec2 position) {
+    m_Interactables.emplace_back(position, GetAtlasIndex(type), type);
+}
+
+std::optional<InteractableType::Enumeration> Interactables::CanInteract(const Player& player) {
+    glm::vec2 playerPosition = player.Position;
+    if (m_CachedPosition == playerPosition && m_CachedAngle == player.Rotation) {
+        if (m_CachedIndex >= Count()) {
+            return std::nullopt;
+        }
+
+        return std::optional<InteractableType::Enumeration>(m_Interactables[m_CachedIndex].Type);
+    }
+
+    m_CachedPosition = playerPosition;
+    m_CachedAngle = player.Rotation;
+    m_CachedIndex = -1;
+
+    glm::vec2 forward{ glm::cos(glm::radians(player.Rotation )), -glm::sin(glm::radians(player.Rotation )) };
+
+    float maxDot = 0.0f;
+    for (size_t i = 0; i < m_Interactables.size(); i++) {
+        auto delta = m_Interactables[i].Position - m_CachedPosition;
+        float squaredDistance = glm::dot(delta, delta);
+        
+        // Check squared distance to save sqrt
+        if (squaredDistance > (s_InteractionRadius * s_InteractionRadius)) {
+            continue;
+        }
+
+        // Divide by squared distance, so closer interactables are preferred
+        float dot = glm::dot(delta, forward) / (squaredDistance);
+        if (dot > maxDot) {
+            m_CachedIndex = i;
+            maxDot = dot;
+        }
+    }
+
+    if (m_CachedIndex == -1) {
+        return std::nullopt;
+    }
+
+    return std::optional<InteractableType::Enumeration>(m_Interactables[m_CachedIndex].Type);
+}
+
+InteractionResult Interactables::Interact(const Player& player) {
+    auto type = CanInteract(player);
+    
+    if (!type) {
+        return { InteractionResult::Type::None, ""};
+    }
+    
+    auto interaction = GetInteraction(type.value());
+    if (!interaction) {
+        return { InteractionResult::Type::None, "" };
+    }
+    
+    return interaction();
+}
+
+void Interactables::UpdateRender(std::span<Sprite> sprites, std::span<Core::Model> models) {
+    for (size_t i = 0; i < Count(); i++) {
+        const Interactable& interactable = m_Interactables[i];
+        
+        // Update on Raycaster-layer
+        sprites[i].Scale = GetScale(interactable.Type);
+        sprites[i].AtlasIndex = interactable.AtlasIndex;
+        sprites[i].FlipTexture = false;
+
+        sprites[i].Position.x = interactable.Position.x;
+        sprites[i].Position.y = interactable.Position.y;
+        sprites[i].Position.z = CalculatePositionZ(interactable.Type, sprites[i].Scale.z);
+        sprites[i].WorldPosition = sprites[i].Position;
+    }
+}
+
+glm::vec3 Interactable::Scale() const {
+    return GetScale(Type);
+}
---
+++ Raycaster-App/Source/Interactables.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "Entities.h"
+
+#include "Core.h"
+#include "Core/Debug/Debug.h"
+
+#include <span>
+#include <optional>
+
+namespace InteractableType {
+    enum Enumeration : uint8_t {
+        Light = 0,
+        Barrel,
+        ENUMERATION_MAX = Barrel,
+    };
+}
+
+struct Interactable {
+    glm::vec2 Position{};
+
+    uint32_t AtlasIndex = 0;
+    InteractableType::Enumeration Type;
+
+    glm::vec3 Scale() const;
+};
+
+struct InteractionResult {
+    enum class Type {
+        None = 0,
+        Debug,
+    };
+    Type Type;
+    std::string_view Data;
+};
+
+class Interactables {
+public:
+    void Init();
+    void Shutdown();
+
+    void Add(InteractableType::Enumeration type, glm::vec2 position);
+
+    std::optional<InteractableType::Enumeration> CanInteract(const Player& player);
+    InteractionResult Interact(const Player& player);
+
+    void UpdateRender(std::span<Sprite> sprites, std::span<Core::Model> models);
+
+    inline size_t Count() const { return m_Interactables.size(); }
+
+    inline const std::span<const Interactable> Get() const { return m_Interactables; }
+    inline const Interactable& operator[](size_t index) {
+        RC_ASSERT(index < Count());
+        return m_Interactables[index];
+    }
+private:
+    std::vector<Interactable> m_Interactables;
+
+    glm::vec2 m_CachedPosition{};
+    float m_CachedAngle{};
+    size_t m_CachedIndex{};
+
+    static constexpr float s_InteractionRadius = 1.0f;
+};
