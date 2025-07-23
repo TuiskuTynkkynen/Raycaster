f2f60ec7b9123ac2b91aa11d3ae2aa0dd771d33f
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-07-23 21:59:51 +0300

    Updated Core Timestep function to constexpr

--- Raycaster-Core/Source/Core/Base/Timestep.h
+++ Raycaster-Core/Source/Core/Base/Timestep.h
@@ -5,11 +5,11 @@ namespace Core {
 	private :
 		float m_Time;
 	public:
-		Timestep(float time = 0.0f) : m_Time(time) {}
+		constexpr Timestep(float time = 0.0f) : m_Time(time) {}
 
-		operator float() const { return m_Time; }
-		float GetSeconds() const { return m_Time; }
-		float GetMilliseconds() const { return m_Time * 1000; }
+		constexpr operator float() const { return m_Time; }
+		constexpr float GetSeconds() const { return m_Time; }
+		constexpr float GetMilliseconds() const { return m_Time * 1000; }
 	};
 
 }
