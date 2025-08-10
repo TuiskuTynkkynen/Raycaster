45a42fac0dae768e352ca98df4234f7b496fc3ad
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-08-10 20:02:14 +0300

    Implemented basic Player death mechanic

--- Raycaster-App/Source/RaycasterScene.cpp
+++ Raycaster-App/Source/RaycasterScene.cpp
@@ -36,6 +36,8 @@ void RaycasterScene::Init(){
 }
 
 void RaycasterScene::Reinit() {
+    m_Paused = false;
+
     m_Player.Position = glm::vec3((float)m_Map.GetWidth() / 2, (float)m_Map.GetHeight() / 2, 0.5f);
     m_Player.Scale = m_Map.GetScale() * 0.4f;
     m_Player.Rotation = 90.0f;
@@ -545,6 +547,10 @@ void RaycasterScene::UseItem(Core::Timestep deltaTime) {
 void RaycasterScene::DamageAreas(std::span<const LineCollider> attack, float thickness, float damage) {
     const bool hit = Algorithms::LineCollisions(m_Player.Position, attack, thickness) != glm::vec2(0.0f);
     m_Player.Health -= damage * hit;
+    
+    if (m_Player.Health <= 0.0f) {
+        m_Paused = true;
+    }
 }
 
 static Core::Model CreateBillboard(std::shared_ptr<Core::Shader> shader, std::shared_ptr<Core::Texture2D> texture, glm::vec3 scale, uint32_t atlasIndex) {
