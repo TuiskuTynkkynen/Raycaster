3e00bf779e09e12a1657855eab4f2b6530d73821
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-08-21 23:20:07 +0300

    Implemented LineOfSightCondition

--- Raycaster-App/Source/EnemyBehaviour.h
+++ Raycaster-App/Source/EnemyBehaviour.h
@@ -32,6 +32,11 @@ struct Transition {
 ///////////////////////////////////////////////////////////////////////////////
 bool LineOfSight(const Context& context, glm::vec2 start, glm::vec2 end);
 
+template<bool negate = false>
+bool LineOfSightCondtion(const Context& context, Enemy& enemy){
+    return LineOfSight(context, enemy.Position, context.PlayerPosition) ^ negate;
+}
+
 template<float distance, bool negate = false>
 bool DistanceCondition(const Context& context, Enemy& enemy) {
     return (glm::length(enemy.Position - context.PlayerPosition) < distance) ^ negate;
