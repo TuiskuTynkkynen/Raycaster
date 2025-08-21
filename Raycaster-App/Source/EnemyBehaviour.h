c233831fe17cee7641d97543e890bd3c8e14deda
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-08-21 23:21:09 +0300

    Implemented templated compiletime And function for Conditions

--- Raycaster-App/Source/EnemyBehaviour.h
+++ Raycaster-App/Source/EnemyBehaviour.h
@@ -30,6 +30,16 @@ struct Transition {
 ///////////////////////////////////////////////////////////////////////////////
 //                                Conditions                                 //
 ///////////////////////////////////////////////////////////////////////////////
+template<Condition A, Condition... B>
+bool And(const Context& context, Enemy& enemy){
+    return (A(context, enemy) && And<B...>(context, enemy));
+}
+
+template<typename=void>
+bool And(const Context& context, Enemy& enemy){
+    return true;
+}
+
 bool LineOfSight(const Context& context, glm::vec2 start, glm::vec2 end);
 
 template<bool negate = false>
