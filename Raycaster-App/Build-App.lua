ec374030626a36fdeb7df76500625b8cccc9705f
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-12-22 17:29:20 +0200

    Updated build scripts to use new dependency project and paths

--- Raycaster-App/Build-App.lua
+++ Raycaster-App/Build-App.lua
@@ -1,75 +1,61 @@
 project "Raycaster-App"
-   kind "ConsoleApp"
-   language "C++"
-   cppdialect "C++20"
-   targetdir "Binaries/%{cfg.buildcfg}"
-   staticruntime "off"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    targetdir "Binaries/%{cfg.buildcfg}"
+    staticruntime "off"
 
-   files { 
-      "Source/**.h", "Source/**.cpp",
-      "Source/Shaders/**.glsl",
-      "Source/Textures/**",
-      "Source/Libraries/GLFW/**",
-      "Source/Libraries/glad/**"
-   }
+    files { 
+        "Source/**.h", "Source/**.cpp",
+        "Source/Shaders/**.glsl",
+        "Source/Textures/**",
+        "Source/Libraries/GLFW/**",
+        "Source/Libraries/glad/**"
+    }
 
-   includedirs
-   {
-      "Source",
+    includedirs
+    {
+        "Source",
 
-	  "Source/Shaders",
-	  "Source/Textures",
+	    "Source/Shaders",
+	    "Source/Textures",
 
-	  -- Include Core
-	  "../Raycaster-Core/Source",
-          "../Raycaster-Core/Source/Libraries/",
-          "../Raycaster-Core/Source/Libraries/freetype",
-          "../Raycaster-Core/Source/Libraries/dlg",
-          "../Raycaster-Core/Source/Libraries/GLFW/include",
-          "../Raycaster-Core/Source/Libraries/glad/include",
-          "../Raycaster-Core/Source/Libraries/glad/src",
-   }	
+	    -- Include Core
+	    "../Raycaster-Core/Source",
+        "../Raycaster-Core/Dependencies/GLFW/include",
+	    "../Raycaster-Core/Dependencies/glad/include",
+	    "../Raycaster-Core/Dependencies/glm",
+	    "../Raycaster-Core/Dependencies/FreeType/include",
+	    "../Raycaster-Core/Dependencies/miniaudio",
+	    "../Raycaster-Core/Dependencies/utils",
+    }	
 
-   libdirs
-   {
-      "../Raycaster-Core/Source/Libraries/freetype",
-      "../Raycaster-Core/Source/Libraries/GLFW/include",
-      "../Raycaster-Core/Source/Libraries/GLFW/lib-vc2022",
-   }
+    links
+    {
+        "Raycaster-Core",
+        "Raycaster-Dependencies",
+    }
 
-   links
-   {
-      "Raycaster-Core",
-      "glfw3",
-      "freetype",
-   }
+    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+    objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
 
-   filter { "system:windows" }
-      links { "OpenGL32" }
+    filter "system:windows"
+        systemversion "latest"
+        defines { "WINDOWS" }
 
-   filter { "system:not windows" }
-      links { "GL" }
-	
-   targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
-   objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+    filter "configurations:Debug"
+        defines { "DEBUG", "LOG_ENABLE" }
+        runtime "Debug"
+        symbols "On"
 
-   filter "system:windows"
-       systemversion "latest"
-       defines { "WINDOWS" }
+    filter "configurations:Release"
+        defines { "RELEASE", "LOG_ENABLE", "LOG_PREFIX_LEVEL=1"  }
+        runtime "Release"
+        optimize "On"
+        symbols "On"
 
-   filter "configurations:Debug"
-       defines { "DEBUG", "LOG_ENABLE" }
-       runtime "Debug"
-       symbols "On"
-
-   filter "configurations:Release"
-       defines { "RELEASE", "LOG_ENABLE", "LOG_PREFIX_LEVEL=1"  }
-       runtime "Release"
-       optimize "On"
-       symbols "On"
-
-   filter "configurations:Dist"
-       defines { "DIST" }
-       runtime "Release"
-       optimize "On"
-       symbols "Off"
+    filter "configurations:Dist"
+        defines { "DIST" }
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
--- Raycaster-Core/Build-Core.lua
+++ Raycaster-Core/Build-Core.lua
@@ -1,52 +1,54 @@
 project "Raycaster-Core"
-   kind "StaticLib"
-   language "C++"
-   cppdialect "C++20"
-   targetdir "Binaries/%{cfg.buildcfg}"
-   staticruntime "off"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    targetdir "Binaries/%{cfg.buildcfg}"
+    staticruntime "off"
 
-   files { 
-      "Source/**.h", "Source/**.cpp",
-      "Source/Libraries/**",
-      "Source/Libraries/GLFW/**",
-      "Source/Libraries/glad/**",
-   }
+    files { 
+        "Source/**.h", "Source/**.cpp",
+    }
 
-   includedirs
-   {
-      "Source",
-      "Source/Libraries/",
-      "Source/Libraries/freetype/",
-      "Source/Libraries/dlg/",
-      "Source/Libraries/GLFW/include",
-      "Source/Libraries/glad/include",
-      "Source/Libraries/glad/src",
-   }
+    includedirs
+    {
+        "Source",
+        "Dependencies/GLFW/include",
+	    "Dependencies/glad/include",
+	    "Dependencies/glm",
+	    "Dependencies/FreeType/include",
+	    "Dependencies/miniaudio",
+	    "Dependencies/utils",
+    }
+
+    links {
+        "Raycaster-Dependencies"
+    }
    
-   -- Required by miniaudio
-   filter { "system:linux" }
-      links { "lpthread", "lm", "ldl" }
-   
-   targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
-   objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+    objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
 
-   filter "system:windows"
-       systemversion "latest"
-       defines { }
+    filter "system:windows"
+        systemversion "latest"
+        defines { }
 
-   filter "configurations:Debug"
-       defines { "DEBUG", "LOG_ENABLE"}
-       runtime "Debug"
-       symbols "On"
+    filter "configurations:Debug"
+        defines { "DEBUG", "LOG_ENABLE"}
+        runtime "Debug"
+        symbols "On"
 
-   filter "configurations:Release"
-       defines { "RELEASE", "LOG_ENABLE", "LOG_PREFIX_LEVEL=1"}
-       runtime "Release"
-       optimize "On"
-       symbols "On"
+    filter "configurations:Release"
+        defines { "RELEASE", "LOG_ENABLE", "LOG_PREFIX_LEVEL=1"}
+        runtime "Release"
+        optimize "On"
+        symbols "On"
 
-   filter "configurations:Dist"
-       defines { "DIST" }
-       runtime "Release"
-       optimize "On"
-       symbols "Off"
+    filter "configurations:Dist"
+        defines { "DIST" }
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
+
+
+group "Core/Dependencies"
+    include "Dependencies/Build-Dependencies.lua"
+group ""
