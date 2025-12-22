8a4048ca3abc766e46972848017252491fd0177f
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-12-22 17:28:32 +0200

    Updated setup scripts to generate needed glad header and source files

--- Scripts/Setup-Linux.sh
+++ Scripts/Setup-Linux.sh
@@ -1,5 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 pushd ..
-Vendor/Binaries/Premake/Linux/premake5 --cc=clang --file=Build.lua gmake2
+
+if [ ! -d ./Core/Dependencies/glad/include/ ]; then
+    echo Generating glad...
+    pushd ./Core/Dependencies/glad_generator
+
+    python -m venv venv
+    source ./venv/bin/activate
+
+    python -m pip install -r requirements.txt
+    python -m pip install -e .
+    glad --api gl:compatibility=3.3 --out-path ../glad
+
+    deactivate
+    rm -rf ./venv
+    popd
+fi
+
+./Vendor/premake/bin/release/premake5 --cc=clang --file=Build.lua gmake
+
 popd
--- Scripts/Setup-Windows.bat
+++ Scripts/Setup-Windows.bat
@@ -1,6 +1,19 @@
 @echo off
 
 pushd ..
+
+if not exist .\Raycaster-Core\Dependencies\glad\include\ (
+    echo Generating glad...
+    pushd .\Raycaster-Core\Dependencies\glad_generator
+
+    python -m venv venv
+    cmd /c ".\venv\Scripts\activate && python -m pip install -r requirements.txt && python -m pip install -e . && glad --api gl:compatibility=3.3 --out-path ..\glad && deactivate"
+
+    rmdir /S /Q .\venv
+    popd
+)
+
 Vendor\Binaries\Premake\Windows\premake5.exe --file=Build.lua vs2022
+
 popd
 pause
