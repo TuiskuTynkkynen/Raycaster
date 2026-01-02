cdf4f2d3546cc58696d1fcbbd4886182ee6a6fd3
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2026-01-02 17:03:28 +0200

    Fixed build system downloading stb_image header even if it already exits

--- Raycaster-Core/Dependencies/stb_image.lua
+++ Raycaster-Core/Dependencies/stb_image.lua
@@ -1,6 +1,9 @@
 function AddStbImage()
-	os.mkdir("utils/stb")
-	os.execute("curl \"https://raw.githubusercontent.com/nothings/stb/f1c79c02822848a9bed4315b12c8c8f3761e1296/stb_image.h\" -o utils/stb/stb_image.h")
+	local stat = os.stat("utils/stb/stb_image.h")
+	if stat == nil or stat.size == 0 then
+		os.mkdir("utils/stb")
+		os.execute("curl \"https://raw.githubusercontent.com/nothings/stb/f1c79c02822848a9bed4315b12c8c8f3761e1296/stb_image.h\" -o utils/stb/stb_image.h")
+	end
 	
 	files
 	{
