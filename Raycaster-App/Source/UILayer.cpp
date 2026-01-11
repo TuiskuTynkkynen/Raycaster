c4cbc3ce381caa0ad2f7368a02264f0a98319514
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2026-01-11 18:18:02 +0200

    Implemented UI for changing key bindings

--- Raycaster-App/Source/UILayer.cpp
+++ Raycaster-App/Source/UILayer.cpp
@@ -1,6 +1,7 @@
 #include "UILayer.h"
 
-#include "RaycasterScene.h"
+#include "Keybinds.h"
+
 #include "Core/UI/UI.h"
 
 void UILayer::OnAttach() {
@@ -52,29 +53,91 @@ void UILayer::OnUpdate(Core::Timestep deltaTime) {
 
     RaycasterScene::State sceneState = scene.GetState();
     if (sceneState != RaycasterScene::State::Running) {
-        Core::UI::BeginContainer(Core::UI::PositioningType::Offset, { -0.025f, 0.0f }, { 1.0f, 1.0f }, { 0.1f, 0.1f, 0.1f, 0.5f });
-        Core::UI::Text(sceneState == RaycasterScene::State::Dead ? "You died!" : "Paused", glm::vec2{ 0.5f, 0.25f }, glm::vec4(1.0f));
-
-        if (sceneState >= RaycasterScene::State::Dead && Core::UI::Button("Restart", glm::vec2{ 0.5f, 0.25f })) {
-            Core::Application::PushEvent<Restart>();
+        if (m_ShowKeyBinds) {
+            KeyBindsScreen(scene);
+        } else {
+            PauseScreen(scene);
         }
-
-        if (sceneState == RaycasterScene::State::Paused && Core::UI::Button("Continue", glm::vec2{ 0.5f, 0.25f })) {
-            Core::Application::PushEvent<Resume>();
-        }
-
-        if (Core::UI::Button("Quit", glm::vec2{ 0.5f, 0.25f })) {
-            Core::Application::PushEvent<Core::ApplicationClose>();
-        }
-        Core::UI::EndContainer();
     }
 
     Core::UI::End(deltaTime);
 }
 
+void UILayer::PauseScreen(const RaycasterScene& scene) {
+    RaycasterScene::State sceneState = scene.GetState();
+
+    Core::UI::BeginContainer(Core::UI::PositioningType::Offset, { -0.025f, 0.0f }, { 1.0f, 1.0f }, { 0.1f, 0.1f, 0.1f, 0.5f });
+    Core::UI::Text(sceneState == RaycasterScene::State::Dead ? "You died!" : "Paused", glm::vec2{ 0.5f, 0.25f }, glm::vec4(1.0f));
+
+    if (sceneState >= RaycasterScene::State::Dead && Core::UI::Button("Restart", glm::vec2{ 0.5f, 0.2f })) {
+        Core::Application::PushEvent<Restart>();
+    }
+
+    if (sceneState == RaycasterScene::State::Paused && Core::UI::Button("Continue", glm::vec2{ 0.5f, 0.2f })) {
+        Core::Application::PushEvent<Resume>();
+    }
+
+    if (Core::UI::Button("Key Binds", glm::vec2{ 0.5f, 0.2f })) {
+        m_ShowKeyBinds = true;
+    }
+
+    if (Core::UI::Button("Quit", glm::vec2{ 0.5f, 0.2f })) {
+        Core::Application::PushEvent<Core::ApplicationClose>();
+    }
+    Core::UI::EndContainer();
+}
+
+void UILayer::KeyBindsScreen(const RaycasterScene& scene) {
+    static std::array<glm::vec4, 3> deselectedColours = { Core::UI::DefaultColours[0], Core::UI::DefaultColours[0], Core::UI::DefaultColours[0] };
+    static std::array<glm::vec4, 3> selectedColours = { Core::UI::DefaultColours[2], Core::UI::DefaultColours[2], Core::UI::DefaultColours[2] };
+    bool allDefault = true;
+
+Core::UI::BeginContainer(Core::UI::PositioningType::Offset, { -0.025f, 0.0f }, { 1.0f, 1.0f }, { 0.1f, 0.1f, 0.1f, 0.5f });
+
+    Core::UI::Text("Key Binds", { 0.5f, 0.125f }, glm::vec4(1.0f));
+
+    static float scrollOffset = 0;
+    Core::UI::BeginScrollContainer(scrollOffset, { 0.75f, 0.65f }, true, 1.0f, glm::vec4(0.0f), glm::vec4(0.0f));
+    for (uint32_t i = 0; i < s_KeyBinds.size(); i++) {
+        Core::UI::BeginContainer(Core::UI::PositioningType::Offset, { -0.025f, 0.0f }, { 0.925f, 0.125f }, glm::vec4(0.0f), Core::UI::LayoutType::Horizontal);
+        Core::UI::Text(s_KeyBinds[i].GetName(), { 0.5f, 1.0f });
+
+        bool isSelected = m_SelectedKeyBind == i;
+        if (Core::UI::Button(Core::Input::KeyCodeToString(s_KeyBinds[i].KeyCode), { 0.2f, 1.0f }, isSelected ? selectedColours : Core::UI::DefaultColours)) {
+            m_SelectedKeyBind = isSelected ? -1 : i;
+        }
+
+        bool isDefault = s_KeyBinds[i].KeyCode == s_KeyBinds[i].GetDefaultKeyCode();
+        allDefault &= isDefault;
+
+        if (Core::UI::Button("Reset", { 0.2f, 1.0f }, isDefault ? deselectedColours : Core::UI::DefaultColours, isDefault ? selectedColours : Core::UI::DefaultTextColours)) {
+            s_KeyBinds[i].Reset();
+        }
+        Core::UI::EndContainer();
+    }
+        Core::UI::ScrollBar(scrollOffset, { 0.05f, 1.0f });
+    Core::UI::EndScrollContainer();
+
+    Core::UI::BeginContainer({ 0.5f, 0.125f }, glm::vec4(0.0f), Core::UI::LayoutType::Horizontal);
+        if (Core::UI::Button("Reset to Defaults", { 0.5f, 1.0f }, allDefault ? deselectedColours : Core::UI::DefaultColours, allDefault ? selectedColours : Core::UI::DefaultTextColours)) {
+            for (KeyBind& bind : s_KeyBinds) {
+                bind.Reset();
+            }
+        }
+
+        if (Core::UI::Button("Back", { 0.5f, 1.0f })) {
+            m_ShowKeyBinds = false;
+        }
+    Core::UI::EndContainer();
+
+Core::UI::EndContainer();
+}
+
 void UILayer::OnEvent(Core::Event& event) {
     Core::EventDispatcher dispatcer(event);
     dispatcer.Dispatch<Core::WindowResize>(std::bind(&UILayer::OnWindowResizeEvent, this, std::placeholders::_1));
+    dispatcer.Dispatch<Core::KeyReleased>(std::bind(&UILayer::OnKeyReleased, this, std::placeholders::_1));
+
     Core::UI::OnEvent(event);
 }
 
@@ -82,5 +145,25 @@ bool UILayer::OnWindowResizeEvent(Core::WindowResize& event) {
     m_ViewPortWidth = event.GetWidth();
     m_ViewPortHeight = event.GetHeight();
 
+    return false;
+}
+
+bool UILayer::OnKeyReleased(Core::KeyReleased& event) {
+    if (!m_ShowKeyBinds) {
+        return false;
+    }
+
+    if (event.GetKeyCode() == RC_KEY_ESCAPE) {
+        m_ShowKeyBinds = m_SelectedKeyBind != -1;
+        m_SelectedKeyBind = -1;
+        return true;
+    }
+
+    if (m_SelectedKeyBind != -1 && m_SelectedKeyBind < s_KeyBinds.size()) {
+        s_KeyBinds[m_SelectedKeyBind].KeyCode = event.GetKeyCode();
+        m_SelectedKeyBind = -1;
+        return true;
+    }
+    
     return false;
 }
--- Raycaster-App/Source/UILayer.h
+++ Raycaster-App/Source/UILayer.h
@@ -1,17 +1,26 @@
 #pragma once
 
+#include "RaycasterScene.h"
+
 #include "Core/Base/Layer.h"
 #include "Core/Events/WindowEvent.h"
 
 class UILayer : public Core::Layer {
 public:
-	void OnAttach() override;
-	void OnDetach() override;
-	void OnUpdate(Core::Timestep deltaTime) override;
-	void OnEvent(Core::Event& event) override;
+    void OnAttach() override;
+    void OnDetach() override;
+    void OnUpdate(Core::Timestep deltaTime) override;
+    void OnEvent(Core::Event& event) override;
 private:
-	uint32_t m_ViewPortWidth = -1;
-	uint32_t m_ViewPortHeight = -1;
+    uint32_t m_ViewPortWidth = -1;
+    uint32_t m_ViewPortHeight = -1;
 
-	bool OnWindowResizeEvent(Core::WindowResize& event);
+    bool m_ShowKeyBinds = false;
+    uint32_t m_SelectedKeyBind = -1;
+
+    void PauseScreen(const RaycasterScene& scene);
+    void KeyBindsScreen(const RaycasterScene& scene);
+
+    bool OnKeyReleased(Core::KeyReleased& event);
+    bool OnWindowResizeEvent(Core::WindowResize& event);
 };
