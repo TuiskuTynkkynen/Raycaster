edf4c934433433b8114c4b7c616a17489893f190
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2026-01-11 17:30:47 +0200

    Deduplicated utf8/wide string conversions from Clipboard.cpp

--- Raycaster-Core/Source/Core/Base/Clipboard.cpp
+++ Raycaster-Core/Source/Core/Base/Clipboard.cpp
@@ -2,22 +2,7 @@
 
 #include "Application.h"
 #include "Window.h"
-
-
 #include "Platform.h"
-#ifdef PLATFORM_WINDOWS
-	#include <Windows.h>
-#elif defined(PLATFORM_LINUX)
-	#ifndef __clang__
-		#error "Only clang is supported on Linux"
-	#endif 
-
-	#include <codecvt>
-	#include <locale>
-
-	#pragma clang diagnostic push
-	#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-#endif
 
 #include <GLFW/glfw3.h>
 
@@ -27,16 +12,8 @@ namespace Core {
 	}
 
 	std::wstring Clipboard::GetClipboardWide() {
-#ifdef PLATFORM_WINDOWS
 		const char* clipboard = glfwGetClipboardString(static_cast<GLFWwindow*>(Application::GetWindow().GetWindowPointer()));
-		int32_t utf8Size = static_cast<int32_t>(std::strlen(clipboard));
-		int32_t wideSize = MultiByteToWideChar(CP_UTF8, 0, clipboard, utf8Size, NULL, 0);
-		std::wstring wide(wideSize, 0);
-		MultiByteToWideChar(CP_UTF8, 0, clipboard, utf8Size, wide.data(), wideSize);
-		return wide;
-#else
-		return std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(glfwGetClipboardString(static_cast<GLFWwindow*>(Application::GetWindow().GetWindowPointer())));
-#endif
+		return UTF8ToWide(clipboard);
 	}
 
 	template <>
@@ -63,16 +40,8 @@ namespace Core {
 	}
 
 	void Clipboard::SetClipboardWide(std::wstring_view str) {
-#ifdef PLATFORM_WINDOWS
-		int32_t wideSize = static_cast<int32_t>(str.size());
-		int32_t utf8Size = WideCharToMultiByte(CP_UTF8, 0, str.data(), wideSize, NULL, 0, NULL, NULL);
-		std::string utf8(utf8Size, 0);
-		WideCharToMultiByte(CP_UTF8, 0, str.data(), wideSize, &utf8[0], utf8Size, NULL, NULL);
-		
+		std::string utf8 = WideToUTF8(str);
 		glfwSetClipboardString(static_cast<GLFWwindow*>(Application::GetWindow().GetWindowPointer()), utf8.c_str());
-#else
-		glfwSetClipboardString(static_cast<GLFWwindow*>(Application::GetWindow().GetWindowPointer()), std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(str.data()).c_str());
-#endif 
 	}
 
 	template <>
@@ -90,7 +59,3 @@ namespace Core {
 		SetClipboardWide(str);
 	}
 }
-
-#ifdef PLATFORM_LINUX
-	# pragma clang diagnostic pop
-#endif
