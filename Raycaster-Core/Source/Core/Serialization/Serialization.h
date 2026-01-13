3dc7bf216014eee0e3266cb04936279d45df406c
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2026-01-13 23:57:59 +0200

    Added Archive class to Serialization system

--- Raycaster-Core/Source/Core/Serialization/Serialization.h
+++ Raycaster-Core/Source/Core/Serialization/Serialization.h
@@ -2,20 +2,32 @@
 
 #include <vector>
 #include <span>
+#include <cstddef>
 
 namespace Core::Serialization {
-    class SerializationToken {};
-    
+    class Archive;
+
     template <typename T>
-    T Deserialize(std::span<SerializationToken>) = delete;
-    
+    T Deserialize(Archive& archive) = delete;
+
     template <typename T>
-    std::vector<SerializationToken> Serialize(const T&) = delete;
-    
+    bool Serialize(const T& value, Archive& archive) = delete;
+
     template <typename T>
-    concept Serializable = requires(T a)
+    concept Serializable = requires(T a, Archive& archive)
     {
-        Deserialize<T>({});
-        Serialize<T>(a);
+        Deserialize<T>(archive);
+        Serialize<T>(a, archive);
+    };
+
+    class Archive {
+    public:
+        template <Serializable T>
+        T Read();
+        std::span<std::byte> Read(size_t size);
+
+        template <Serializable T>
+        bool Write(const T& value);
+        bool Write(std::span<std::byte>);
     };
 }
