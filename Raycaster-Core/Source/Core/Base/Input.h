a11817b11a4f768cd4fefe017f016c54daf373dd
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2026-02-04 19:47:48 +0200

    Implemented Input system IsValidKeyCode and IsValidButtonCode functions

--- Raycaster-Core/Source/Core/Base/Input.h
+++ Raycaster-Core/Source/Core/Base/Input.h
@@ -23,7 +23,35 @@ namespace Core::Input {
 	};
 	void SetCursorMode(CursorMode mode);
 
+	constexpr bool IsValidKeyCode(uint32_t keyCode);
+	constexpr bool IsValidButtonCode(uint32_t buttonCode);
+
 	std::wstring_view KeyCodeToString(uint32_t keyCode);
 	std::wstring_view ButtonCodeToString(uint32_t mouseButtonCode);
 }
 
+constexpr bool Core::Input::IsValidKeyCode(uint32_t keyCode) {
+    switch (keyCode) {
+    case  32: return true;
+    case  39: return true;
+    case  59: return true;
+    case  61: return true;
+    case  96: return true;
+    case 161: return true;
+    case 162: return true;
+    }
+
+    if (keyCode >= 44 && keyCode <= 57) return true;
+    if (keyCode >= 65 && keyCode <= 93) return true;
+    if (keyCode >= 256 && keyCode <= 269) return true;
+    if (keyCode >= 280 && keyCode <= 284) return true;
+    if (keyCode >= 290 && keyCode <= 314) return true;
+    if (keyCode >= 320 && keyCode <= 336) return true;
+    if (keyCode >= 340 && keyCode <= 348) return true;
+
+    return false;
+}
+
+constexpr bool Core::Input::IsValidButtonCode(uint32_t buttonCode) {
+    return buttonCode >= RC_MOUSE_BUTTON_1 && buttonCode <= RC_MOUSE_BUTTON_8;
+}
